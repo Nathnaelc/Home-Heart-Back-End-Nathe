0ae40d5fd93e58d7b0063682c79f3ff908da1b96
Ethan Pineda <joshuapineda66@gmail.com>
2023-07-25 23:59:02 -0700

    backend for comments section done

--- models/medicalProfessional.js
+++ models/medicalProfessional.js
@@ -17,6 +17,40 @@ class MedicalProfessional {
         }
     }
 
-}; 
+    static async fetchMedicalProfessionalComments(professional_id) {
+        if (!professional_id) {
+            throw new BadRequestError("No professional id provided");
+        }
+    
+        try {
+            // Get all of the comments for this medical professional along with user_id
+            const { rows } = await db.pool.query(
+                'SELECT user_id, review_text, date_posted, rating FROM user_reviews WHERE professional_id = $1',
+                [professional_id]
+            );
+    
+            // Create an array to store the promises for fetching user details
+            const promises = rows.map(async (row) => {
+                // Fetch user's first and last name from the users table based on user_id
+                const userData = await db.pool.query(
+                    'SELECT first_name, last_name FROM users WHERE user_id = $1',
+                    [row.user_id]
+                );
+                // Assign the user's first name and last name to the row object
+                row.first_name = userData.rows[0].first_name;
+                row.last_name = userData.rows[0].last_name;
+                return row; // Return the updated row object
+            });
+    
+            // Wait for all the promises to resolve
+            const updatedRows = await Promise.all(promises);
+    
+            return updatedRows;
+        } catch (err) {
+            console.log("error:", err);
+            throw new Error("Failed to fetch medical professional comments");
+        }
+    }
+}
 
 module.exports = MedicalProfessional;
--- routes/authMedProf.js
+++ routes/authMedProf.js
@@ -26,5 +26,28 @@ router.get("/getMedicalProfessionalById/:professional_id", async (req, res) => {
     }
 }); 
 
+router.get("/comments/:professional_id", async (req, res) => {
+    
+        console.log("req.params.professional_id: ", req.params.professional_id)
+        try {
+            const result = await MedicalProfessional.fetchMedicalProfessionalComments(req.params.professional_id);
+            if (result == null) {
+                return res.status(200).json({
+                message: "No professional found",
+                result: []
+            });
+        }
+        else {
+            console.log("result: ", result);
+            return res.status(200).json({
+            message: "Professional successfully got",
+            result: result
+        })};
+        } catch (err) {
+            console.log("HERE")
+            return; 
+        }
+})
+
 
 module.exports = router;
