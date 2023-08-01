172a0b00f18d070badba0ed6df4c3f85d635340d
Ethan Pineda <joshuapineda66@gmail.com>
2023-08-01 10:14:36 -0700

    finished implementing SMS reminders on the backend

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "homeheart_db",
+  database: "homeheart_database",
   password: "postgres",
   port: 5432,
 });
